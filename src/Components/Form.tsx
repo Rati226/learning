5c8a1cd895ea3f85be13761800d9ae2abdd29286
Rati226 <Ratigupta226@gmail.com>
2022-09-23 02:11:54 +0530

    first commit

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,46 +1,63 @@
 import React from 'react';
 
-import { Typography ,RadioGroup, Button, TextField ,Container , Radio } from '@mui/material';
+import { Typography , Card, CardContent, Grid, TextField , Button} from '@mui/material';
+import { CheckBox } from '@mui/icons-material';
 
 
 const Form = () => {
   return (
     <div>
-      <Container>
-        <Typography variant="h1" component="h2">
-          form making
-        </Typography>
-        ;
-        <form>
-          <TextField
-            id="outlined-basic"
-            fullWidth
-            label="Outlined"
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-basic"
-            fullWidth
-            required
-            label="Outlined"
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-basic"
-            fullWidth
-            required
-            label="Outlined"
-            variant="outlined"
-          />
-        </form>
-        <RadioGroup>
-          <Radio name="radio-buttons" inputProps={{ 'aria-label': 'A' }} />
-          <Radio name="radio-buttons" inputProps={{ 'aria-label': 'B' }} />
-        </RadioGroup>
-        <Button type="submit" variant="outlined">
-          Submit
-        </Button>
-      </Container>
+      <Typography variant="h1" component="h4">
+        form making
+      </Typography>
+      ;
+      <Card style={{maxWidth:450 , margin:"0 auto",padding: "20px 5px" , color : "red"}}>
+        <CardContent>
+          <Grid container spacing={1}>
+            <Grid xs={12} sm={6} item>
+              <TextField
+                label="First Name"
+                placeholder="First Name"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} sm={6} item>
+              <TextField
+                label="Last Name"
+                placeholder="Last Name"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12}  item>
+              <TextField
+                label="email"
+                placeholder="name@gmail.com"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                label="Message"
+                placeholder="Enter Your Message"
+                multiline
+                rows={4}
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+             <Button type='submit' variant='contained' color='primary' fullWidth>Submit</Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </div>
   );
 };
