b76cfe5a0b27964e44f35cd72ae093a21d96876b
Rati226 <66001344+Rati226@users.noreply.github.com>
2022-09-23 09:27:56 +0530

    Update Form.tsx

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,11 +1,137 @@
 import React from 'react';
 
-import { Typography ,RadioGroup, Button, TextField ,Container , Radio } from '@mui/material';
+import { Typography , Card, CardContent,Container, Grid, TextField , Button,Box, List, ListItem, ListItemAvatar, Avatar, ListItemText } from '@mui/material';
 
 
 const Form = () => {
   return (
-    <div>
+    <div>     
+      <Typography variant="h1" component="h4">
+        form making
+      </Typography>
+      ;
+      <Card
+        style={{
+          maxWidth: 450,
+          margin: '0 auto',
+          padding: '20px 5px',
+          color: 'red',
+        }}
+      >
+        <CardContent>
+          <Grid container spacing={1}>
+            <Grid xs={12} sm={6} item>
+              <TextField
+                label="First Name"
+                placeholder="First Name"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} sm={6} item>
+              <TextField
+                label="Last Name"
+                placeholder="Last Name"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                label="email"
+                placeholder="name@gmail.com"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                label="Message"
+                placeholder="Enter Your Message"
+                multiline
+                rows={4}
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+      <Box sx={{ width: '400px', bgcolor: '#efefef' }}>
+        <List>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar></Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="person 1" />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar></Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Person 2" />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar></Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="person 3" />
+          </ListItem>
+        </List>
+      </Box>
+      <Card variant = "outlined"
+        style={{
+          maxWidth: 450,
+          margin: '0 auto',
+          padding: '20px 5px',
+          color: 'red',
+        }}
+      >
+        <CardContent>
+          <Grid container spacing={1}>
+            <Grid xs={12} sm={6} item>
+        
+               <Typography variant="h6" component="h6">user 4</Typography>
+            </Grid>
+            <Grid xs={12} item>
+              <Typography variant="h6" component="h6">
+                user 1
+              </Typography>
+            </Grid>
+            <Grid xs={12} item>
+              <Typography variant="h6" component="h6">
+                user 2
+              </Typography>
+            </Grid>
+            <Grid xs={12} item>
+              <Typography variant="h6" component="h6">
+                user 3
+              </Typography>
+            </Grid>
+            <Grid xs={12} item>
+              <Typography variant="h6" component="h6">
+                user 4
+              </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+      
+      
       <Container>
         <Typography variant="h1" component="h2">
           form making
@@ -41,8 +167,11 @@ const Form = () => {
           Submit
         </Button>
       </Container>
+      
+      
     </div>
   );
 };
 
 export default Form;
+
